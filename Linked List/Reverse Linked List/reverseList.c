cc7d1cee1329b247fb0e45f5eb2ef6a817392d46
dhx20150812 <1793516813@qq.com>
2017-03-21 21:26:48 +0800

    Create reverseList.c

---
+++ Linked List/Reverse Linked List/reverseList.c
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    if(!head) return NULL;
+    if(!head->next) return head;
+    struct ListNode *res = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return res;
+}
