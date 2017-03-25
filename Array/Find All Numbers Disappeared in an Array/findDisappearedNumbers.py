61d0f84fc3c00f2380155d3fe87223a1f1ffc497
dhx20150812 <1793516813@qq.com>
2017-03-25 21:09:35 +0800

    Create findDisappearedNumbers.py

---
+++ Array/Find All Numbers Disappeared in an Array/findDisappearedNumbers.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # 解题思路见隔壁C语言写法
+        cnt = 0
+        for x in nums:
+            x = x if x>0 else -x
+            if nums[x-1]>0:
+                cnt += 1
+                nums[x-1] = -nums[x-1]
+        ret = []
+        for index,x in enumerate(nums):
+            if x>0:
+                ret.append(index+1)
+        return ret
