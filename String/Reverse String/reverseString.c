f55d415691d5d5e6ad54a760f434e348d2cda976
dhx20150812 <1793516813@qq.com>
2017-03-26 22:33:38 +0800

    Create reverseString.c

---
+++ String/Reverse String/reverseString.c
@@ -0,0 +1,9 @@
+char* reverseString(char* s) {
+    int i,j;char tmp;
+    for(i=0,j=strlen(s)-1;i<j;i++,j--){
+        tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+    return s;
+}
